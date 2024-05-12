4a26dd14ee60a9f178120c725149e177d244fc65
Sharuchakalaka7 <ahmed.shahriar343@gmail.com>
2024-05-12 19:35:21 -0400

    Broker comms working! (with tls)

--- main.go
+++ main.go
@@ -1,69 +1,77 @@
 package main
 
 import (
-    "fmt"
-    mqtt "github.com/eclipse/paho.mqtt.golang"
-    "time"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
-var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-    fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
-}
 
-var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
-    fmt.Println("Connected")
-}
 
-var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
-    fmt.Printf("Connect lost: %v", err)
-}
 
-func publish(client mqtt.Client) {
-    num := 10
-    for i := 0; i < num; i++ {
-        text := fmt.Sprintf("Message %d", i)
-        token := client.Publish(TOPIC, 0, false, text)
-        token.Wait()
-        time.Sleep(time.Second)
-    }
-}
-
-func sub(client mqtt.Client) {
-    token := client.Subscribe(TOPIC, 1, nil)
-    token.Wait()
-    fmt.Printf("Subscribed to topic: %s", TOPIC)
+var messageHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("TOPIC: %s\n", msg.Topic())
+	fmt.Printf("MSG: %s\n", msg.Payload())
 }
 
 
 
 
 func main() {
+
+    // Initialize logs
+    mqtt.DEBUG = log.New(os.Stdout, "\tMQTT DEBUG: ", 0)
+    mqtt.ERROR = log.New(os.Stdout, "\tMQTT ERROR: ", 0)
+
+    // Setup client options
     opts := mqtt.NewClientOptions()
 
-    opts.AddBroker(fmt.Sprintf("%s://%s:%d", BROKER_L4, BROKER_ADDR, BROKER_PORT))
+    opts.AddBroker(fmt.Sprintf("%s://%s:%d", BROKER_SCHEME, BROKER_HOST, BROKER_PORT))
+
     opts.SetClientID(CLIENT_ID)
     opts.SetUsername(CLIENT_USER)
     opts.SetPassword(CLIENT_PSWD)
 
-    opts.SetDefaultPublishHandler(messagePubHandler)
-    opts.OnConnect = connectHandler
-    opts.OnConnectionLost = connectLostHandler
+    opts.SetKeepAlive(60 * time.Second)
 
-    fmt.Println("Client options configured")
+    // Set the message callback handler
+    opts.SetDefaultPublishHandler(messageHandler)
+    // opts.SetPingTimeout(30 * time.Second)
 
-    client := mqtt.NewClient(opts)
-    if token := client.Connect(); token.Wait() && token.Error() != nil {
+    // Initialize client
+    c := mqtt.NewClient(opts)
+    if token := c.Connect(); token.Wait() && token.Error() != nil {
 	panic(token.Error())
     }
 
-    fmt.Println("Client initialized")
+    // Subscribe to a topic
+    if token := c.Subscribe(TOPIC, 0, nil); token.Wait() && token.Error() != nil {
+	fmt.Println(token.Error())
+	os.Exit(1)
+    }
 
-    sub(client)
-    fmt.Println("Client subscribed")
+    time.Sleep(6 * time.Second)
 
-    publish(client)
-    fmt.Println("Client published")
+    // Publish a message
+    token := c.Publish(TOPIC, 0, false, "Hello from Golang!")
+    token.Wait()
 
-    client.Disconnect(15000)
+    time.Sleep(6 * time.Second)
+
+    /*
+
+    // Unscribe
+    if token := c.Unsubscribe(TOPIC); token.Wait() && token.Error() != nil {
+	fmt.Println(token.Error())
+	os.Exit(1)
+    }
+
+    // Disconnect
+    c.Disconnect(250)
+    time.Sleep(1 * time.Second)
+    */
+    time.Sleep(60*time.Second)
 }
-
--- params.go
+++ params.go
@@ -1,23 +1,13 @@
-/*
-APP_ID="a953fdf7"
-APP_SECRET="q7be7eda1d1d9d10"
-
-API_ENDPOINT="https://y4d72f30.ala.us-east-1.emqxsl.com:8443/api/v5"
-
-BROKER_ADDR="y4d72f30.ala.us-east-1.emqxsl.com"
-BROKER_TLS_PORT=8883
-*/
-
 package main
 
 const (
-    BROKER_ADDR = "y4d72f30.ala.us-east-1.emqxsl.com"
-    BROKER_PORT = 1883
-    BROKER_L4   = "tcp"
+    BROKER_SCHEME = "ssl"
+    BROKER_HOST   = "y4d72f30.ala.us-east-1.emqxsl.com"
+    BROKER_PORT   = 8883
 
-    CLIENT_ID   = "rpi-1"
-    CLIENT_USER = "UoGuelph-IoT"
-    CLIENT_PSWD = "UoGuelph-IoT-Sensors"
+    CLIENT_ID     = "Shah-Laptop"
+    CLIENT_USER   = "UoGuelph-IoT"
+    CLIENT_PSWD   = "UoGuelph-IoT-Sensors"
 
-    TOPIC = "sensor-1"
+    TOPIC         = "gas-quantity/src-id"
 )
